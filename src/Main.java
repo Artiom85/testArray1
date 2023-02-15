5a12b821c11e4904775b3c5ae069c0715303fa19
Artiom85 <stratulatartemii2012@gmail.com>
2023-02-15 16:03:13 +0000

    HW 1 exercitiu

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+    int[] date = { 23, 34, 45, 56,67,445};
+
+        System.out.println(Arrays.toString(date));
+    }
+}
